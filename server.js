f7bf9e45a628e37faf59981412a6ea4315abf24b
Mohammad Rashidi <rickykuma@Mohammads-MacBook-Pro.local>
2022-10-31 23:14:23 -0400

    1

--- server.js
+++ server.js
@@ -7,69 +7,75 @@
  * * Name: Mohammad Rashidi Khorsand   Student ID: 134713213 Date: 30/10/22 * 
  * ********************************************************************************/
 
-const express = require("express");
-const path = require("path");
-const { allowedNodeEnvironmentFlags } = require("process");
-express.urlencoded({ extended: true }); 
-middleware (using ,app.use());
+const HTTP_PORT = process.env.PORT || 8080;
 
+
+const path = require("path");
+
+
+const data = require("./modules/officeData.js");
+
+
+
+const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
 
- const HTTP_PORT = process.env.PORT || 8080;
- app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.get("/audio", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/audio.html"));
+});
 
-
- app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname, "views/home.html"));
- });
-
- app.get("/audio", (req,res)=>{
-    res.sendFile(path.join(__dirname, "views/audio.html"));
- });
-
- app.get("/video", (req,res)=>{
-    res.sendFile(path.join(__dirname, "views/video.html"));
- });
- 
- app.get("/list", (req,res)=>{
-    res.sendFile(path.join(__dirname, "views/list.html"));
- });
-  
- app.get("/table", (req,res)=>{
-    res.sendFile(path.join(__dirname, "views/table.html"));
- });
-
- app.get("/storefront", (req,res)=>{
-   res.sendFile(path.join(__dirname, "views/storefront.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
 
- app.get("/PartTimer", (req,res)=>{
-    data.getPartTimers().then((data)=>{
-        res.json(data);
+app.get("/video", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/video.html"));
+});
+
+app.get("/list", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/list.html"));
+});
+
+app.get("/table", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/table.html"));
+});
+
+app.get("/storefront", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/storefront.html"));
+});
+
+app.get("/PartTimer", (req, res) => {
+  data.getPartTimers().then((data) => {
+    res.json(data);
+  });
+});
+
+app.get("/employee/:employeeNum", (req, res) => {
+  data
+    .getEmployeeByNum(req.params.employeeNum)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json({ message: "no results" });
     });
- });
+});
 
- app.get("/employee/:employeeNum",(req,res)=>{
-    data.getEmployeeByNum(req.params.employeeNum).then((data)=>{
-        res.json(data);
-    }).catch((err)=>{
-        res.json({message:"no results"});
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+});
+
+data
+  .initialize()
+  .then(function () {
+    app.listen(HTTP_PORT, () => {
+      console.log("app listening on port: " + HTTP_PORT);
     });
- });
-
-
- app.use((req,res)=>{
-    res.status(404).send("Page Not Found");
- });
-
- data.initialize().then(function(){
-   app.listen(HTTP_PORT, function(){
-      console.log("app listening on: " +  HTTP_PORT)
-   });
- }).catch(function(err){
-   console.log("Unable to start server: " + err);
- });
-
- app.listen(HTTP_PORT);
+  })
+  .catch(function (err) {
+    console.log("unable to start server: " + err);
+  });
